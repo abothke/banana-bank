7dd61b623f729270d23afe82a6cc1a8fe55b4d90
abothke <andrebothke@gmail.com>
2024-01-20 11:35:54 +0100

    done

---
+++ src/assets/components/bananaBank/BananaBank.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react"
+import './bananabank.css'
+import logo from '../../img/logo.png'
+import card from '../../img/bbcard.png'
+
+const BananaBank = ({accountBalance, setAccountBalance}) => {
+    const [accountData, setAccountData] = useState({
+        entryValue: '',
+        accountBalance: 0
+    })
+
+
+    const depositOrWithdraw = (option) => {
+        let inputValue = document.querySelector("#balance").value
+        if(option === "withdraw"){
+            setAccountData({
+                ...accountData,
+                entryValue: inputValue,
+                accountBalance: accountData.accountBalance - Number(inputValue)
+            })
+        } else {
+            setAccountData({
+                ...accountData,
+                entryValue: inputValue,
+                accountBalance: accountData.accountBalance + Number(inputValue)
+            })
+        }
+    }
+    return ( 
+        <>
+        <img src={logo} alt="" />
+        <form>
+            <div id="bankAccount">
+                <p>Willkommen, Herr Banane!</p>
+                <h3>Girokonto</h3>
+                <img src={card} alt="" />
+                <h4>IBAN DE 1234 1234 1234 00</h4>
+                <div id="accountDetails">
+                    <div id="balance">
+                    <p>Kontostand</p>
+                           <h2 id="accountBalance">{accountData.accountBalance} EUR</h2>
+                    </div>
+                
+            </div>
+            </div>
+            <input
+            type="number"
+            name="balance"
+            id="balance"
+            placeholder="Wieviel EUR möchten Sie ein oder auszahlen?" />
+            <div id="buttons">
+            <button onClick={(e) => {
+                e.preventDefault();
+                depositOrWithdraw("deposit")
+                }}>Einzahlen</button>
+            <button onClick={(e) => {
+                e.preventDefault();
+                depositOrWithdraw("withdraw")
+                }}>Auszahlen</button>
+                </div>
+        </form>
+        </>
+     );
+}
+ 
+export default BananaBank;
