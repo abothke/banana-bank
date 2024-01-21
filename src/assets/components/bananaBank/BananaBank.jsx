3db33b0e42333b21c308a5f22c474f0b38324492
abothke <andrebothke@gmail.com>
2024-01-21 11:56:24 +0100

    chore: Added comments to the code

--- src/assets/components/bananaBank/BananaBank.jsx
+++ src/assets/components/bananaBank/BananaBank.jsx
@@ -4,25 +4,25 @@ import logo from '../../img/logo.png'
 import card from '../../img/bbcard.png'
 
 const BananaBank = () => {
-    const [accountData, setAccountData] = useState({
-        entryValue: '',
-        accountBalance: 0
+    const [accountData, setAccountData] = useState({ // Usestate wird hier verwendet um den Kontostand zu speichern und zu aktualisieren
+        entryValue: '', // entryValue wird hier verwendet um den Wert des Eingabefeldes zu speichern
+        accountBalance: 0 // accountBalance wird hier verwendet um den Kontostand zu speichern
     })
 
 
     const depositOrWithdraw = (option) => {
-        let inputValue = document.querySelector("#balance").value
-        if(option === "withdraw"){
+        let inputValue = document.querySelector("#balance").value // inputValie ist hier das Inputfeld mit der ID balance
+        if(option === "withdraw"){ // Wenn der Button Auszahlen geklickt wird, wird der Kontostand um den Wert des Eingabefeldes verringert
             setAccountData({
-                ...accountData,
-                entryValue: inputValue,
-                accountBalance: accountData.accountBalance - Number(inputValue)
+                ...accountData, // ...accountData wird hier verwendet um die Daten des Kontos zu behalten
+                entryValue: inputValue,  // entryValue wird hier mit dem Wert des Eingabefeldes aktualisiert
+                accountBalance: accountData.accountBalance - Number(inputValue) // accountBalance wird hier mit dem aktuellen Kontostand minus dem Wert des Eingabefeldes aktualisiert
             })
         } else {
             setAccountData({
                 ...accountData,
                 entryValue: inputValue,
-                accountBalance: accountData.accountBalance + Number(inputValue)
+                accountBalance: accountData.accountBalance + Number(inputValue) // accountBalance wird hier mit dem aktuellen Kontostand plus dem Wert des Eingabefeldes aktualisiert
             })
         }
     }
@@ -54,11 +54,11 @@ const BananaBank = () => {
             <div id="buttons">
             <button onClick={(e) => {
                 e.preventDefault();
-                depositOrWithdraw("deposit")
+                depositOrWithdraw("deposit") // Wenn der Button Einzahlen geklickt wird, wird der Kontostand um den Wert des Eingabefeldes erhöht, weil depositOrWithdraw("deposit") aufgerufen wird
                 }}>Einzahlen</button>
             <button onClick={(e) => {
                 e.preventDefault();
-                depositOrWithdraw("withdraw")
+                depositOrWithdraw("withdraw") // Wenn der Button Auszahlen geklickt wird, wird der Kontostand um den Wert des Eingabefeldes verringert, weil depositOrWithdraw("withdraw") aufgerufen wird
                 }}>Auszahlen</button>
                 </div>
         </form>
